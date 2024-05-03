6a38c6c41c0aa01563d5c2c995a7e2b734e25fbc
Jinyoung Kim <jinyoungkim@Jinyoungs-MacBook-Air.local>
2024-05-03 19:09:45 +0900

    initial setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  const String font = "Pretendard";
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.web);
+
+  runApp(
+    ProviderScope(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          fontFamily: font,
+        ),
+        home: ScreenTypeLayout.builder(
+          tablet: (BuildContext context) => MyApp(), //Web view
+          mobile: (BuildContext context) => MyApp(), //Mobile view
+        ),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,6 +30,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text('ddd'),
+      ),
+    );
   }
 }
