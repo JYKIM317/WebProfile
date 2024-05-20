23a22d23359b7ef4874d4964a8214aaaddc7ed88
Jinyoung Kim <jinyoungkim@Jinyoungs-MacBook-Air.local>
2024-05-20 20:29:17 +0900

    build introduce widget

--- lib/presentation/intro/intro_view.dart
+++ lib/presentation/intro/intro_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class IntroView extends StatefulWidget {
   final bool isMobile;
@@ -15,7 +16,44 @@ class _IntroViewState extends State<IntroView> {
       child: Container(
         width: double.infinity,
         height: MediaQuery.of(context).size.height,
-        color: Colors.black,
+        decoration: const BoxDecoration(
+          color: Colors.transparent,
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.black,
+              Colors.transparent,
+            ],
+          ),
+        ),
+        child: Center(
+          child: AnimatedTextKit(
+            repeatForever: true,
+            animatedTexts: [
+              TyperAnimatedText(
+                '안녕하세요!',
+                textStyle: TextStyle(
+                  fontSize: widget.isMobile ? 48 : 62,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                speed: const Duration(milliseconds: 300),
+                curve: Curves.easeInToLinear,
+              ),
+              TyperAnimatedText(
+                '보러 와주셔서 감사합니다.',
+                textStyle: TextStyle(
+                  fontSize: widget.isMobile ? 34 : 62,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                speed: const Duration(milliseconds: 240),
+                curve: Curves.fastEaseInToSlowEaseOut,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
